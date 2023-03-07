bd04605e41da20d8df2a5dda0cddc1e81fbe1210
Egor <plekhanov.e@icloud.com>
2023-03-07 12:54:12 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 from telebot import *
-
+import random
 bot = TeleBot('6123239125:AAFrcgVlnt5tgzGUTwyxCaXdsQhmtU6shi0')
 
 @bot.message_handler(commands=['start'])
@@ -7,6 +7,12 @@ def push(message):
     chseKey = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn = types.KeyboardButton('Жми!')
     chseKey.add(btn)
-    bot.send_message(message.from_user.id, 'Жми, чтобы узнать, какой ты Арсений!', reply_markup=chseKey)
+    return bot.send_message(message.from_user.id, 'Жми, чтобы узнать, какой ты Арсений!', reply_markup=chseKey)
+
+@bot.message_handler(func=lambda message: True)
+def stickers(file):
+    rnd = str(random.randint(1,9))
+    sti = open('/stickers/'+rnd, 'rb')
+    bot.send_sticker('@what_Arseny_are_you_bot', sti)
 
 bot.infinity_polling()
