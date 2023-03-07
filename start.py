ecf0901cc1f923617d4b776ea1e98f1340ad8a7a
Egor <plekhanov.e@icloud.com>
2023-03-07 11:54:39 +0300

    Update start.py

--- start.py
+++ start.py
@@ -3,10 +3,10 @@ from telebot import *
 bot = TeleBot('6123239125:AAFrcgVlnt5tgzGUTwyxCaXdsQhmtU6shi0')
 
 @bot.message_handler(commands=['start'])
-def start(message):
+def choose(message):
     chseKey = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn = types.KeyboardButton('Жми!')
     chseKey.add(btn)
     bot.send_message(message.from_user.id, 'Жми, чтобы узнать, какой ты Арсений!', reply_markup=chseKey)
 
-bot.polling(none_stop=True, interval=0)
+bot.infinity_polling()
