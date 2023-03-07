8dba173f3ebe4cf11d62f39970494a495364c61c
Egor <plekhanov.e@icloud.com>
2023-03-07 11:39:05 +0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,11 @@
+from telebot import *
+
+bot = TeleBot("6123239125:AAFrcgVlnt5tgzGUTwyxCaXdsQhmtU6shi0")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    chseKey = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn = types.KeyboardButton('Жми!')
+    chseKey.add(btn)
+    bot.send_message(message.from_user.id, 'Жми, чтобы узнать, какой ты Арсений!', reply_markup=chseKey)
+    
